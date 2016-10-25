b53b1aa74dd55b88344f89af8a7a8d3d214eb65f
tadakazu <tadakazu1972@gmail.com>
2016-10-25 23:20:37 +0900

    add Inquiry3Activity

--- app/src/main/java/tadakazu1972/firetranslater/HomeActivity.java
+++ app/src/main/java/tadakazu1972/firetranslater/HomeActivity.java
@@ -59,6 +59,13 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        mView.findViewById(R.id.btnInquiry3).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(mActivity, Inquiry3Activity.class);
+                startActivity(intent);
+            }
+        });
         mView.findViewById(R.id.btnBack).setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
