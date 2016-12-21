ca143dece95c9958fe0b309489dac7e7d051f7aa
tadakazu1972 <tadakazu1972@gmail.com>
2016-12-21 16:20:02 +0900

    add タイ語以降、すべて

--- app/src/main/java/tadakazu1972/firetranslater/MainActivity.java
+++ app/src/main/java/tadakazu1972/firetranslater/MainActivity.java
@@ -72,6 +72,126 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        //タイ語
+        mView.findViewById(R.id.btnThai).setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //選択した言語番号保存　タイ語＝5
+                SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(mActivity);
+                sp.edit().putString("language", "5").apply();
+                //ホーム画面へ
+                Intent intent = new Intent(mActivity, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        //タガログ語
+        mView.findViewById(R.id.btnPhilippine).setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //選択した言語番号保存　タガログ語＝6
+                SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(mActivity);
+                sp.edit().putString("language", "6").apply();
+                //ホーム画面へ
+                Intent intent = new Intent(mActivity, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        //フランス語
+        mView.findViewById(R.id.btnFrench).setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //選択した言語番号保存　フランス語＝7
+                SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(mActivity);
+                sp.edit().putString("language", "7").apply();
+                //ホーム画面へ
+                Intent intent = new Intent(mActivity, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        //インドネシア語
+        mView.findViewById(R.id.btnIndonesia).setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //選択した言語番号保存　インドネシア語＝8
+                SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(mActivity);
+                sp.edit().putString("language", "8").apply();
+                //ホーム画面へ
+                Intent intent = new Intent(mActivity, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        //ポルトガル語
+        mView.findViewById(R.id.btnPortuguese).setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //選択した言語番号保存　ポルトガル語＝9
+                SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(mActivity);
+                sp.edit().putString("language", "9").apply();
+                //ホーム画面へ
+                Intent intent = new Intent(mActivity, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        //ベトナム語
+        mView.findViewById(R.id.btnVietnum).setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //選択した言語番号保存　ベトナム語＝a
+                SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(mActivity);
+                sp.edit().putString("language", "a").apply();
+                //ホーム画面へ
+                Intent intent = new Intent(mActivity, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        //ドイツ語
+        mView.findViewById(R.id.btnGermany).setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //選択した言語番号保存　ドイツ語＝b
+                SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(mActivity);
+                sp.edit().putString("language", "b").apply();
+                //ホーム画面へ
+                Intent intent = new Intent(mActivity, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        //スペイン語
+        mView.findViewById(R.id.btnEspanyol).setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //選択した言語番号保存　スペイン語＝c
+                SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(mActivity);
+                sp.edit().putString("language", "c").apply();
+                //ホーム画面へ
+                Intent intent = new Intent(mActivity, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        //ロシア語
+        mView.findViewById(R.id.btnRussian).setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //選択した言語番号保存　ロシア語＝d
+                SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(mActivity);
+                sp.edit().putString("language", "d").apply();
+                //ホーム画面へ
+                Intent intent = new Intent(mActivity, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        //マレー語
+        mView.findViewById(R.id.btnMalay).setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //選択した言語番号保存　マレー語＝e
+                SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(mActivity);
+                sp.edit().putString("language", "e").apply();
+                //ホーム画面へ
+                Intent intent = new Intent(mActivity, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
 
         //日本語
         mView.findViewById(R.id.btnJapanese).setOnClickListener(new OnClickListener(){
